bfb5dff5850876be115e9b4bf79270c4473ef9b3
Ayur Dorzhiev <ayurra@mail.ru>
2020-12-27 13:30:22 +0300

    toggle,popupConsult,sendForm

--- src/modules/sendForms.js
+++ src/modules/sendForms.js
@@ -1,28 +1,76 @@
 const sendForms = () => {
-//     const errorMessage = 'ошибка..',
-//         loadMessage = 'идет отправка',
-//         successMesage = 'отправлено';
-//     const form = document.getElementById('form1');
+const errorMessage = 'ошибка..',
+        loadMessage = 'Идет отпарвка',
+        successMesage = 'Отправлено';
+    const form = document.querySelectorAll('form');
 
-//     const statusMessage = document.createElement('div');
-//     statusMessage.textContent = 'тут будет сообщение';
-//     statusMessage.style.cssText = 'font-size: 2rem; color: #85be32;';
-    
-//     form.addEventListener('submit', (event) => {
-//         event.preventDefault();
-         
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 2rem; color: black;';
 
-//         const request = new XMLHttpRequest();
-//         request.open('POST', './server.php');
-//         request.setRequestHeader('Content-Type', 'multipart/form-data');
-//         const formData = new FormData(form);
-//         request.send(formData);
+    form.forEach(item => item.addEventListener('submit', (event) => {
+        event.preventDefault();
+        item.appendChild(statusMessage); // добавляем к определенной форме сообщение
+        statusMessage.textContent = loadMessage;
+        const formData = new FormData(item); // записываем данные с определенной формы
+        let body = {};
+        formData.forEach((val, key) => {
+            body[key] = val;
+        });
 
-//     request.addEventListener('redystatechandge', ()=>{
-//         statusMessage.textContent = loadMessage;
-//     });
-    
-//   });
+        const popup = document.querySelector('.popup');
+        postData(body)
+            .then((response) => {
+                if (response.status !== 200) {
+                    throw new Error('status network not 200');
+                }
+                statusMessage.textContent = successMesage;
+                setTimeout(() => {
+                    popup.style.display = 'none';
+                    statusMessage.textContent = '';
+                    [...document.querySelectorAll('input')].forEach(item => item.value = '');
+                }, 3000);
+            }, (error) => {
+                statusMessage.textContent = errorMessage;
+                setTimeout(() => {
+                    popup.style.display = 'none';
+                    statusMessage.textContent = '';
+                    [...document.querySelectorAll('input')].forEach(item => item.value = '');
+                }, 3000);
+            });
+    }));
+
+    const postData = (body) => {
+        return fetch( './server.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+            credentials: 'include',
+            cache: 'default'
+        })
+        
+  };
+    const formPhone = document.querySelectorAll('.phone-user');
+    formPhone.forEach((item) => {
+        item.addEventListener('input', (event) => {
+            const target = event.target;
+            if (target.matches('input')) {
+                target.value = target.value.replace(/(?!^\+)\D/g, '');
+            }
+        });
+    });
+
+    const formName = document.querySelectorAll('.user-name');
+    formName.forEach((item) => {
+        item.addEventListener('input', (event) => {
+            const target = event.target;
+            if (target.matches('input')) {
+                target.value = target.value.replace(/(?![а-яА-Я])\D|[0-9]/g, '');
+            }
+        });
+    });
+
  
 };
 
