f8b3f5eee88a5fec7ff04577429a22edad42a607
Ayur Dorzhiev <ayurra@mail.ru>
2020-12-30 22:43:35 +0300

    diplom finish

--- src/modules/formsValid.js
+++ src/modules/formsValid.js
@@ -6,12 +6,14 @@ const formsValid = () => {
     phones.forEach((e) => {
         e.addEventListener('input', () => {
 
-            e.setAttribute('pattern', '[+][0-9]{7,11}');
+            e.setAttribute('pattern', '[+][0-9]{11}');
             if (!e.value) {
                 e.setAttribute('placeholder', '+7XXXXXXXXXX');
+            } else if () {
+                
             } else {
                 e.setAttribute('placeholder', '+7(___)___-__-__');
-            };
+            }
         });
 
     });
